38b2348db240a9afd07d555ef8e752d8fd504841
dev-qa <varopn@gmail.com>
2021-09-20 14:50:31 +0300

    1.0

--- Joyn.de Tests/lib/Locators/AccountPage/locators.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    accountPageMainInfoTab: '//*[@id="__next"]/div/div[2]/div[1]/nav/ul/a[2]',
-    accountPageInfoTabEmail: '//*[@id="__next"]/div/div[2]/div[2]/section/div/section/table/tbody/tr[1]/td[2]',
-    accountPageInfoTabSex: '//*[@id="__next"]/div/div[2]/div[2]/section/div/section/table/tbody/tr[2]/td[2]',
-    accountPageInfoTabDateOfBirth: '//*[@id="__next"]/div/div[2]/div[2]/section/div/section/table/tbody/tr[3]/td[2]',
-    accountPageEmail: '//*[@id="__next"]/div/div[2]/div[1]/div[2]'
-}
--- Joyn.de Tests/lib/Locators/MainPage/locators.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-        mainPageCountryBannerConfirmButton: '//*[@id="__next"]/div[2]/div[2]/div/div[2]/button',
-        mainPageAccountIcon: '//*[@id="__next"]/div/div/header/a[3]/div/img',
-        mainPageAccountIconAfter: '//*[@id="__next"]/div/div/header/a[2]',
-        mainPagePlusContentButton: '//*[@id="__next"]/div/div/header/button[2]',
-        mainPagePlusContentLoginButton: '//*[@id="__next"]/div[2]/div/div/div[2]/button'
-}
